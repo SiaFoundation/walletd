903c48f03900b2b08f0d8c774e4d5b825f298efd
Alex Freska <alex.freska@gmail.com>
2023-06-30 21:00:48 -0400

    ui: v0.6.0

--- cmd/walletd/web.go
+++ cmd/walletd/web.go
@@ -1,44 +1,19 @@
 package main
 
 import (
-	"embed"
-	"errors"
-	"io/fs"
 	"net"
 	"net/http"
 	"strings"
 
 	"go.sia.tech/jape"
 	"go.sia.tech/walletd/api"
+	"go.sia.tech/web/walletd"
 )
-
-//go:embed dist
-var dist embed.FS
-
-type clientRouterFS struct {
-	fs fs.FS
-}
-
-func (cr *clientRouterFS) Open(name string) (fs.File, error) {
-	f, err := cr.fs.Open(name)
-	if errors.Is(err, fs.ErrNotExist) {
-		return cr.fs.Open("index.html")
-	}
-	return f, err
-}
-
-func createUIHandler() http.Handler {
-	assets, err := fs.Sub(dist, "dist")
-	if err != nil {
-		panic(err)
-	}
-	return http.FileServer(http.FS(&clientRouterFS{fs: assets}))
-}
 
 func startWeb(l net.Listener, node *node, password string) error {
 	renter := api.NewServer(node.cm, node.s, node.wm)
 	api := jape.BasicAuth(password)(renter)
-	web := createUIHandler()
+	web := walletd.Handler()
 	return http.Serve(l, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if strings.HasPrefix(r.URL.Path, "/api") {
 			r.URL.Path = strings.TrimPrefix(r.URL.Path, "/api")
