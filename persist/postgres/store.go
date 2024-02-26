e6cb6230b879b7d49445e40d189e54bfaa5e9584
Nate Maninger <me@n8m.us>
2024-02-26 09:56:34 -0800

    postgres: add postgres store

---
+++ persist/postgres/store.go
@@ -0,0 +1,135 @@
+package postgres
+
+import (
+	"database/sql"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"math"
+	"strings"
+	"time"
+
+	"go.sia.tech/coreutils/chain"
+	"go.uber.org/zap"
+	"lukechampine.com/frand"
+
+	_ "github.com/lib/pq"
+)
+
+type (
+	ConnectionInfo struct {
+		Host     string `json:"host"`
+		Port     int    `json:"port"`
+		User     string `json:"user"`
+		Password string `json:"password"`
+		Database string `json:"database"`
+		SSLMode  string `json:"sslmode"`
+	}
+
+	// A Store is a persistent store that uses a SQL database as its backend.
+	Store struct {
+		db  *sql.DB
+		log *zap.Logger
+
+		updates []*chain.ApplyUpdate
+	}
+)
+
+// String returns a connection string for the given ConnectionInfo.
+func (ci ConnectionInfo) String() string {
+	return fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s", ci.Host, ci.Port, ci.User, ci.Password, ci.Database, ci.SSLMode)
+}
+
+// transaction executes a function within a database transaction. If the
+// function returns an error, the transaction is rolled back. Otherwise, the
+// transaction is committed. If the transaction fails due to a busy error, it is
+// retried up to 10 times before returning.
+func (s *Store) transaction(fn func(*txn) error) error {
+	var err error
+	txnID := hex.EncodeToString(frand.Bytes(4))
+	log := s.log.Named("transaction").With(zap.String("id", txnID))
+	start := time.Now()
+	attempt := 1
+	for ; attempt < maxRetryAttempts; attempt++ {
+		attemptStart := time.Now()
+		log := log.With(zap.Int("attempt", attempt))
+		err = doTransaction(s.db, log, fn)
+		if err == nil {
+			// no error, break out of the loop
+			return nil
+		}
+
+		// return immediately if the error is not a busy error
+		if !strings.Contains(err.Error(), "database is locked") {
+			break
+		}
+		// exponential backoff
+		sleep := time.Duration(math.Pow(factor, float64(attempt))) * time.Millisecond
+		if sleep > maxBackoff {
+			sleep = maxBackoff
+		}
+		log.Debug("database locked", zap.Duration("elapsed", time.Since(attemptStart)), zap.Duration("totalElapsed", time.Since(start)), zap.Stack("stack"), zap.Duration("retry", sleep))
+		jitterSleep(sleep)
+	}
+	return fmt.Errorf("transaction failed (attempt %d): %w", attempt, err)
+}
+
+// Close closes the underlying database.
+func (s *Store) Close() error {
+	return s.db.Close()
+}
+
+// doTransaction is a helper function to execute a function within a transaction. If fn returns
+// an error, the transaction is rolled back. Otherwise, the transaction is
+// committed.
+func doTransaction(db *sql.DB, log *zap.Logger, fn func(tx *txn) error) error {
+	start := time.Now()
+	dbtx, err := db.Begin()
+	if err != nil {
+		return fmt.Errorf("failed to begin transaction: %w", err)
+	}
+	defer func() {
+		if err := dbtx.Rollback(); err != nil && !errors.Is(err, sql.ErrTxDone) {
+			log.Error("failed to rollback transaction", zap.Error(err))
+		}
+		// log the transaction if it took longer than txn duration
+		if time.Since(start) > longTxnDuration {
+			log.Debug("long transaction", zap.Duration("elapsed", time.Since(start)), zap.Stack("stack"), zap.Bool("failed", err != nil))
+		}
+	}()
+
+	tx := &txn{
+		Tx:  dbtx,
+		log: log,
+	}
+	if err = fn(tx); err != nil {
+		return err
+	} else if err = tx.Commit(); err != nil {
+		return fmt.Errorf("failed to commit transaction: %w", err)
+	}
+	return nil
+}
+
+// ResetDatabase resets the database to its initial state. This should only be
+// used for testing.
+func (s *Store) ResetDatabase() error {
+	_, err := s.db.Exec("DROP SCHEMA public CASCADE;CREATE SCHEMA public;")
+	return err
+}
+
+// OpenDatabase creates a new SQLite store and initializes the database. If the
+// database does not exist, it is created.
+func OpenDatabase(config ConnectionInfo, log *zap.Logger) (*Store, error) {
+	db, err := sql.Open("postgres", config.String())
+	if err != nil {
+		return nil, err
+	}
+	store := &Store{
+		db:  db,
+		log: log,
+	}
+	if err := store.init(); err != nil {
+		return nil, fmt.Errorf("failed to initialize database: %w", err)
+	}
+	return store, nil
+}
