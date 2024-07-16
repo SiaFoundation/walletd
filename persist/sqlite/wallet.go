4dc470558712abb5ea277c477d70d5fd43a9b78a
Nate Maninger <me@n8m.us>
2024-07-16 09:45:42 -0700

    sqlite: remove forced wallet index

--- persist/sqlite/wallet.go
+++ persist/sqlite/wallet.go
@@ -657,7 +657,7 @@ func getWalletEvents(tx *txn, id wallet.ID, offset, limit int) (events []wallet.
 	}
 
 	const eventsQuery = `SELECT ev.id, ev.event_id, ev.maturity_height, ev.date_created, ci.height, ci.block_id, ev.event_type, ev.event_data
-	FROM events ev INDEXED BY events_maturity_height_id_idx -- force the index to prevent temp-btree sorts
+	FROM events ev
 	INNER JOIN event_addresses ea ON (ev.id = ea.event_id)
 	INNER JOIN wallet_addresses wa ON (ea.address_id = wa.address_id)
 	INNER JOIN chain_indices ci ON (ev.chain_index_id = ci.id)
