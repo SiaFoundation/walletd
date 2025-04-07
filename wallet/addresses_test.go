6d49140689f86657199499a552531e2c0430eff8
Nate <me@n8m.us>
2025-04-07 15:20:35 -0700

    fix flaky tests

--- wallet/addresses_test.go
+++ wallet/addresses_test.go
@@ -1,32 +1,17 @@
 package wallet_test
 
 import (
-	"path/filepath"
+	"context"
 	"testing"
 
 	"go.sia.tech/core/types"
-	"go.sia.tech/coreutils"
-	"go.sia.tech/coreutils/chain"
 	"go.sia.tech/walletd/v2/internal/testutil"
-	"go.sia.tech/walletd/v2/persist/sqlite"
 	"go.sia.tech/walletd/v2/wallet"
 	"go.uber.org/zap/zaptest"
 )
 
 func TestAddressUseTpool(t *testing.T) {
 	log := zaptest.NewLogger(t)
-	dir := t.TempDir()
-	db, err := sqlite.OpenDatabase(filepath.Join(dir, "walletd.sqlite3"), log.Named("sqlite3"))
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer db.Close()
-
-	bdb, err := coreutils.OpenBoltChainDB(filepath.Join(dir, "consensus.db"))
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer bdb.Close()
 
 	// mine a single payout to the wallet
 	pk := types.GeneratePrivateKey()
@@ -37,12 +22,9 @@ func TestAddressUseTpool(t *testing.T) {
 	genesisBlock.Transactions[0].SiacoinOutputs = []types.SiacoinOutput{
 		{Address: addr1, Value: types.Siacoins(100)},
 	}
-	store, genesisState, err := chain.NewDBStore(bdb, network, genesisBlock)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	cm := chain.NewManager(store, genesisState)
+	cn := testutil.NewConsensusNode(t, network, genesisBlock, log)
+	cm := cn.Chain
+	db := cn.Store
 
 	wm, err := wallet.NewManager(cm, db, wallet.WithLogger(log.Named("wallet")), wallet.WithIndexMode(wallet.IndexModeFull))
 	if err != nil {
@@ -50,7 +32,7 @@ func TestAddressUseTpool(t *testing.T) {
 	}
 	defer wm.Close()
 
-	mineAndSync(t, cm, db, types.VoidAddress, 1)
+	cn.MineBlocks(t, types.VoidAddress, 1)
 
 	assertSiacoinElement := func(t *testing.T, id types.SiacoinOutputID, value types.Currency, confirmations uint64) {
 		t.Helper()
@@ -111,8 +93,12 @@ func TestAddressUseTpool(t *testing.T) {
 	if _, err := cm.AddV2PoolTransactions(basis, []types.V2Transaction{txn}); err != nil {
 		t.Fatal(err)
 	}
-
+	tip, err := wm.Tip()
+	if err != nil {
+		t.Fatal(err)
+	}
+	wm.Scan(context.Background(), tip) // force reindexing of the tpool
 	assertSiacoinElement(t, txn.SiacoinOutputID(txn.ID(), 1), types.Siacoins(75), 0)
-	mineAndSync(t, cm, db, types.VoidAddress, 1)
+	cn.MineBlocks(t, types.VoidAddress, 1)
 	assertSiacoinElement(t, txn.SiacoinOutputID(txn.ID(), 1), types.Siacoins(75), 1)
 }
